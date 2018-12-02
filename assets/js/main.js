a6a198de420b29d962e6a3ee7da589f68f0d7a84
Vladimir Jimenez <allejo@me.com>
2018-12-01 21:30:08 -0800

    Add OS specific terminal windows for download CLI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,47 @@
 "use strict";
 
-// Replace the default `no-js` class with `js` to help with any JS-specific CSS
-document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, '') + ' js ';
+/**
+ * Detect the OS the browser claims to be.
+ *
+ * @returns {string}
+ */
+function guessOS() {
+    if (navigator.appVersion.indexOf('Win') !== -1) {
+        return 'Windows';
+    }
+
+    if (navigator.appVersion.indexOf('Mac') !== -1) {
+        return 'macOS';
+    }
+
+    if (navigator.appVersion.indexOf('X11') !== -1) {
+        return 'UNIX';
+    }
+
+    if (navigator.appVersion.indexOf('Linux') !== -1) {
+        return 'Linux';
+    }
+
+    return 'Unknown';
+}
+
+/**
+ * Replace a class in the root `<html>` element.
+ *
+ * @param {string|RegExp} find
+ * @param {string} replace
+ */
+function replaceDomClass(find, replace) {
+    document.documentElement.className = document.documentElement.className.replace(find, '') + ' ' + replace + ' ';
+}
+
+// Add some helper classes to the DOM for some fancier features
+//
+//   - Detect the OS from CSS
+//   - Detect JS usage from CSS
+
+replaceDomClass('os-unknown', 'os-' + guessOS().toLowerCase());
+replaceDomClass('no-js', 'js');
 
 // Mobile menu support
 var menuToggle = document.querySelector('#menu-toggle');
