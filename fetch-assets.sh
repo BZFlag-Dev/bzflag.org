140759e6f62e45ebf1ddab10e012997c356b0761
Scott Wichser <blast007@users.sourceforge.net>
2019-03-31 10:55:14 -0500

    Overwrite the fetched assets to ensure we always have the latest versions.

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 mkdir -p assets/videos
-wget -c -O assets/videos/blank.mp4 https://github.com/sampotts/plyr/raw/master/dist/blank.mp4
+wget -O assets/videos/blank.mp4 https://github.com/sampotts/plyr/raw/master/dist/blank.mp4
 
 mkdir -p assets/videos/home
 FILES="bzteaser laser super-bullet guided-missile"
 for FILE in $FILES
 do
-    wget -c -O assets/videos/home/$FILE.mp4 https://f002.backblazeb2.com/file/bzflag-media/videos/home/$FILE.mp4
-    wget -c -O assets/videos/home/$FILE.webm https://f002.backblazeb2.com/file/bzflag-media/videos/home/$FILE.webm
+    wget -O assets/videos/home/$FILE.mp4 https://f002.backblazeb2.com/file/bzflag-media/videos/home/$FILE.mp4
+    wget -O assets/videos/home/$FILE.webm https://f002.backblazeb2.com/file/bzflag-media/videos/home/$FILE.webm
 done
 
 
@@ -16,6 +16,6 @@ mkdir -p assets/videos/documentation/flags/
 FLAGS="laser"
 for FLAG in $FLAGS
 do
-    wget -c -O assets/videos/documentation/flags/$FLAG.mp4 https://f002.backblazeb2.com/file/bzflag-media/videos/documentation/flags/$FLAG.mp4
-    wget -c -O assets/videos/documentation/flags/$FLAG.webm https://f002.backblazeb2.com/file/bzflag-media/videos/documentation/flags/$FLAG.webm
+    wget -O assets/videos/documentation/flags/$FLAG.mp4 https://f002.backblazeb2.com/file/bzflag-media/videos/documentation/flags/$FLAG.mp4
+    wget -O assets/videos/documentation/flags/$FLAG.webm https://f002.backblazeb2.com/file/bzflag-media/videos/documentation/flags/$FLAG.webm
 done
